dd1a77ea9f4d0f71d832feebc655a983e60bd677
Raphael Serra <moreiraraphael33@gmail.com>
2025-09-01 16:49:11 -0300

    Projeto 01 calculando corretamente os 4 primeiros momentos

--- Projeto 02/01/01.f
+++ Projeto 02/01/01.f
@@ -1,5 +1,15 @@
+      REAL FUNCTION N_MEDIA(soma, N, rmedia, iord, NUM_RAND)
+              INTEGER NUM_RAND(1600,2)
+              soma = 0
+              DO i = 1, N
+                soma = soma + ((NUM_RAND(i,2) - rmedia))**iord
+              END DO 
+              N_MEDIA = soma/N 
+      END FUNCTION
+      
       PROGRAM LCG
       INTEGER NUM_RAND(1600, 2)
+      REAL N_MEDIA
 
       WRITE(*,*) 'Digite A'
       READ(*,*) iA
@@ -23,4 +33,22 @@
       DO i=1,N
         WRITE(*,*) NUM_RAND(i,1), NUM_RAND(i,2) 
       END DO
+
+      !Média
+      soma = 0
+      DO i=1,N
+        soma = soma + NUM_RAND(i,2) 
+      END DO 
+      rmedia = soma / N
+      WRITE(*,*) 'A média é: ', rmedia
+
+      !VariÂncia
+      WRITE(*,*)'A variância é: ', N_MEDIA(soma, N, rmedia, 2, NUM_RA
+     &ND)
+      !Assimetria
+      WRITE(*,*)'A assimetria é: ', N_MEDIA(soma, N, rmedia, 3, NUM_RA
+     &ND)
+      !Curtose
+      WRITE(*,*)'A curtose é: ', N_MEDIA(soma, N, rmedia, 4, NUM_RA
+     &ND) 
       END 
