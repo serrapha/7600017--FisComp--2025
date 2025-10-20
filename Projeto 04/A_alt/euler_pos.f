b00da24f0012b812f1474922f0e5ddfdadaaf65f
Raphael Serra <moreiraraphael33@gmail.com>
2025-10-20 10:14:05 -0300

    Pêndulo não-linear

---
+++ Projeto 04/A_alt/euler_pos.f
@@ -0,0 +1,34 @@
+      PROGRAM EULER_POS
+
+      PARAMETER(PI = 3.1415927)
+
+      WRITE(*,*) 'Diga theta_0'
+      READ(*,*) theta
+      WRITE(*,*) 'Eleja delta_t'
+      READ(*,*) delta_t
+
+      OPEN(UNIT=10, FILE='euler_posicao.dat', STATUS='UNKNOWN')
+
+C     Calcular o período
+      T = (2*PI)*(1 + (1/4)*(SIN(theta/2))**2 + 
+     &(9/64)*(SIN(theta/2))**4)
+
+C     Calcular o índice do período
+      i_T = T/delta_t
+
+C     Inicializei o omega explicitamente porque, do contrário, ocorriam
+C     problemas relacionados à valores fantasmas. 
+      omega = 0
+
+
+C     Posição
+      DO i=1,100*(i_T)
+        theta = MOD(theta, PI)
+        temp = omega
+        omega = omega - SIN(theta)*delta_t
+        theta = theta + temp*delta_t  
+        WRITE(10,*) (i*delta_t), theta
+      END DO
+        
+      END PROGRAM 
+
